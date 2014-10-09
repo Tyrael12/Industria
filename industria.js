5c80b454a2ac1b91583aec8fc9e0f0dcb48f998d
Maria <Maria@example.com>
2014-10-09 18:19:38 +0200

    enconstruccion

---
+++ industria.js
@@ -0,0 +1,15 @@
+var boton1 = document.createElement("button");
+boton1.type = "button";
+boton1.classList.add('boton1');
+boton1.textContent = 'Madrid';
+document.body.appendChild(boton1);
+var boton2 = document.createElement("button");
+boton2.type = "button";
+boton2.classList.add('boton2');
+boton2.textContent = 'Barcelona';
+document.body.appendChild(boton2);
+var boton3 = document.createElement("button");
+boton3.type = "button";
+boton3.classList.add('boton3');
+boton3.textContent = 'Ibiza';
+document.body.appendChild(boton3);
